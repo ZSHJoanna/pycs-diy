b51fe4620105469963dccfdb64d9e0278096aaaa
vbonvin <vivien.bonvin@epfl.ch>
2018-08-10 13:36:36 +0200

    updating config file for path issue

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../pycs3'))
 
 # -- General configuration ------------------------------------------------
 
