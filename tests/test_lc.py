94ece1f2233127ef5cfee907f375fa05c827b486
vbonvin <vivien.bonvin@epfl.ch>
2018-08-10 11:33:52 +0200

    Initial test files commit

---
+++ tests/test_lc.py
@@ -0,0 +1,15 @@
+"""
+Testing script, v1
+"""
+
+import os
+import sys
+
+# add the root directory to sys.path, so pycs3 can be imported without installation
+path = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), '../')
+sys.path.append(path)
+
+import pycs3
+
+lc = pycs3.gen.lc.LightCurve()
+print(lc)
