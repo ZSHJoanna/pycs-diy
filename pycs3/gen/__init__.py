2f7819e2e87af32bd64081705ac3802ffb9b10ef
vbonvin <vivien.bonvin@epfl.ch>
2018-08-13 08:09:12 +0200

    bricoling init file for ci...

--- pycs3/gen/__init__.py
+++ pycs3/gen/__init__.py
@@ -1,4 +1,14 @@
 __all__ = ["lc"]
 
 
+# add the pycs package to sys path so submodules can be called directly
+
+import os
+import sys
+import inspect
+
+# Needed only if there's no path pointing to the root directory. Mostly for testing purposes
+path_ = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+sys.path.append(path_)
+
 import lc
