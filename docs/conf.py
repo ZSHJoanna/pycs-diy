6da4c6cc69dedf518e913cb0ee75db352ad72b36
Vivien <viv.moi@gmail.com>
2020-03-29 16:52:45 +0200

    tentative doc fixing

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pycs3'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
