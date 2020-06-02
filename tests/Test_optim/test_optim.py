6ea7ab318ea614535fc488fa14e6f682e31293ce
martin-millon <martin.millon@epfl.ch>
2020-06-02 18:01:22 +0200

    cahnging the assertion

--- tests/Test_optim/test_optim.py
+++ tests/Test_optim/test_optim.py
@@ -58,7 +58,7 @@ class TestOptim(unittest.TestCase):
         print('Best fit', B_best)
         print('A correction', A)
 
-        assert chi2 < 5
+        assert chi2 < 10
 
 
 if __name__ == '__main__':
